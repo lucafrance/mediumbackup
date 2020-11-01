51887d6f53c65f51d851c64466e394277172887f
Luca Franceschini <luca.france@outlook.com>
2020-11-01 17:56:02 +0100

    Fixed medium_backup to mediumbackup

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,7 +3,7 @@ import filecmp
 import os
 import shutil
 
-import mediumbackup
+import mediumbackup as mb
 
 TEST_BACKUP_DIR = os.path.join("tests","backup")
 REFERENCE_STORY_NAME = "2020-10-05 come-aggiungere-i-caratteri-ma"
@@ -12,7 +12,7 @@ class MediumStoriesTest(unittest.TestCase):
     
     def test_story(self):
         for format in ("html", "md"):
-            medium_backup.backup_stories(username="lucafrance", backup_dir=TEST_BACKUP_DIR, format=format)
+            mb.backup_stories(username="lucafrance", backup_dir=TEST_BACKUP_DIR, format=format)
             file_extension = "." + format
             test_file = os.path.join(TEST_BACKUP_DIR, REFERENCE_STORY_NAME) + file_extension
             reference_file = os.path.join("tests", REFERENCE_STORY_NAME) + file_extension
